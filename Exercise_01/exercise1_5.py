1dd215aaef64d51989d3d84c31cefd7577f1096e
Maxim-91 <makc0791@gmail.com>
2025-02-26 19:51:45 +0200

    del

--- Exercise_01/exercise1_5.py
+++
@@ -1,5 +0,0 @@
-# Tulostan tiedot print-komennolla käyttäen \t ja \n
-print ("Etunimi\t Syntymävuosi\t Palkka\t Veroprosentti\n"
-       "Matti\t 1970\t\t\t 2000\t 22.8\n"
-       "Maija\t 1980\t\t\t 2500\t 27.7\n"
-       "Paavo\t 1990\t\t\t 1000\t 19.7")
