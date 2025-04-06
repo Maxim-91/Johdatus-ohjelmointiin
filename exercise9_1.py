6842f3cdd7bb91764822b7693b50d175c6cb1f67
Maxim <112344236+Maxim-91@users.noreply.github.com>
2025-04-06 23:22:00 +0300

    Exercise_09

---
+++ exercise9_1.py
@@ -0,0 +1,10 @@
+# Kutsun funktiota ohjelmassani
+import functions as f
+
+# Luon tekstimuuttujia
+name = "Matti Meikäläinen"
+city = "Helsinki"
+professional = "Ohjelmistosuunnittelija"
+
+# Syötän muuttujia funktioon
+f.show_personal_info(name, city, professional)
---
+++ exercise9_9.py
@@ -0,0 +1,7 @@
+# Rekursiot
+
+import functions as f
+
+path = input("Anna kansiopolut (esim. 'C:\\kansio\\kansio'):\n")
+
+print(f.list_files(path))
